a882c14c714eb92adc8f017f46694092184d1cd3
Sheena Gilliland <sheena.gilliland@gmail.com>
2018-08-19 10:02:21 -0700

    Added state and props for checkpoint work

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>a todo will go here</li>
+      <li>{ this.props.description }</li>
+      <li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+       </li>
     );
   }
 }
